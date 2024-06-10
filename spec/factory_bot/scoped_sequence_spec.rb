e4ea205947fb0824ac3ac9e8677e5a962884b29d
kakubin <wetsand.wfs@gmail.com>
2024-06-10 19:06:31 +0900

    tweak

--- spec/factory_bot/scoped_sequence_spec.rb
+++ spec/factory_bot/scoped_sequence_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe FactoryBot::ScopedSequence do
-  it "has a version number" do
-    expect(FactoryBot::ScopedSequence::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  pending
 end
